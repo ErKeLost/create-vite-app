8e9205415a3da8c0497717ae4eb07173e9318da3
erkelost <1256029807@qq.com>
2022-06-28 18:12:12 +0800

    ✨新增(typescript): 更新工作流

---
+++ .cz-config.js
@@ -0,0 +1,46 @@
+"use strict";
+module.exports = {
+  types: [
+    { value: "✨新增", name: "新增:    新的内容" },
+    { value: "🐛修复", name: "修复:    修复一个Bug" },
+    { value: "📝文档", name: "文档:    变更的只有文档" },
+    { value: "💄格式", name: "格式:    空格, 分号等格式修复" },
+    { value: "♻️重构", name: "重构:    代码重构，注意和特性、修复区分开" },
+    { value: "⚡️性能", name: "性能:    提升性能" },
+    { value: "✅测试", name: "测试:    添加一个测试" },
+    { value: "🔧工具", name: "工具:    开发工具变动(构建、脚手架工具等)" },
+    { value: "⏪回滚", name: "回滚:    代码回退" },
+  ],
+  scopes: [
+    { name: "leetcode" },
+    { name: "javascript" },
+    { name: "typescript" },
+    { name: "Vue" },
+    { name: "node" },
+  ],
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "选择一种你的提交类型:",
+    scope: "选择一个scope (可选):",
+    // used if allowCustomScopes is true
+    customScope: "Denote the SCOPE of this change:",
+    subject: "短说明:\n",
+    body: '长说明，使用"|"换行(可选)：\n',
+    breaking: "非兼容性说明 (可选):\n",
+    footer: "关联关闭的issue，例如：#31, #34(可选):\n",
+    confirmCommit: "确定提交说明?(yes/no)",
+  },
+  allowCustomScopes: true,
+  allowBreakingChanges: ["特性", "修复"],
+  // limit subject length
+  subjectLimit: 100,
+};
---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // 采用 cz 自定义的提交规范, > .cz-config.js
+  extends: ["cz"],
+  rules: {
+    // 自定义规则
+  },
+};
