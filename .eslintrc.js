fceebed96fa4dcf13b9b00b609f0cffe4d683685
erkelost <1256029807@qq.com>
2022-06-28 18:37:34 +0800

    格式(config): update  base config two base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-};
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    "linebreak-style": ["off", "windows"],
+    "no-mixed-spaces-and-tabs": ["off", "windows"],
+    "no-consol": "off"
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  endOfLine: 'auto'
+}
