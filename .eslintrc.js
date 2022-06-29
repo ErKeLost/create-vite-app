8407c5d951cf845e421602bc4a63d22d18bec8d7
erkelost <1256029807@qq.com>
2022-06-29 13:09:47 +0800

    🎨 fix(eslint): 分号问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     indent: ['error', 2, { SwitchCase: 1 }], // error类型，缩进2个空格
     'space-before-function-paren': 0, // 在函数左括号的前面是否有空格
     'eol-last': 0, // 不检测新文件末尾是否有空行
-    semi: ['error', 'always'], // 在语句后面加分号
+    semi: ['error', 'never'], // 在语句后面加分号
     quotes: ['error', 'single'], // 字符串使用单双引号,double,single
     'no-console': ['error', { allow: ['log', 'warn'] }], // 允许使用console.log()
     'arrow-parens': 0,
@@ -31,4 +31,4 @@ module.exports = {
     'comma-dangle': [2, 'never'], // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，always-multiline多行模式必须带逗号，单行模式不能带逗号
     'no-undef': 0
   }
-};
+}
--- packages/vite-cli/index.ts
+++ packages/vite-cli/index.ts
@@ -1,4 +1,4 @@
-const a = 123;
-console.log(a);
-console.log(a + 1);
-export { a };
+const a = 123
+console.log(a)
+console.log(a + 1)
+export { a }
