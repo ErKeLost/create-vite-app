5644e78d4cb7a0b0866a55b5fe42b99b2f9e8a72
erkelost <1256029807@qq.com>
2022-06-30 09:33:00 +0800

    ✅ test(chalk): test chalk version remove to 4.1.0

--- packages/vite-cli/dist/index.js
+++ packages/vite-cli/dist/index.js
@@ -2,6 +2,8 @@
 "use strict";
 exports.__esModule = true;
 exports.a = void 0;
+var chalk_1 = require("chalk");
+console.log((0, chalk_1.red)('12312313213132'));
 var a = 123;
 exports.a = a;
 console.log(a);
--- packages/vite-cli/index.ts
+++ packages/vite-cli/index.ts
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
+import {
+  red as redColor,
+  green as greenColor,
+  yellow as yellowColor,
+  gray as grayColor,
+  blue as blueColor,
+  black as blackColor,
+  magenta as magentaColor,
+  cyan as cyanColor,
+  bgGreen as bgGreenColor,
+  bgRed as bgRedColor,
+  bgYellow as bgYellowColor,
+  bgGray as bgGrayColor,
+  bgBlue as bgBlueColor,
+  bgMagenta as bgMagentaColor,
+  bgCyan as bgCyanColor
+} from 'chalk'
+console.log(redColor('12312313213132'))
 
 const a = 123
 console.log(a)
