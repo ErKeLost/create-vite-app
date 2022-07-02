6e30f7f0ca633f065162e35ed94cf30b8313a78c
erkelost <1256029807@qq.com>
2022-07-02 15:03:01 +0800

    🎨 fix(dist remove): 🎨 fix(git ignore): dist add this
    只能使用 require

--- packages/vite-cli/dist/core/program.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
-const program = new commander_1.Command();
-exports.default = program;
--- packages/vite-cli/dist/core/questions/createProject/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1() {
-    console.log('createProjectQuestions问题');
-}
-exports.default = default_1;
