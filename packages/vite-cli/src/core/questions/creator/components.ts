465dc74b7cb229bf4f9059d8338e8ef86f265471
erkelost <1256029807@qq.com>
2022-07-05 21:24:38 +0800

    🎨 fix(layout): remove layout

--- packages/vite-cli/src/core/questions/creator/components.ts
+++ packages/vite-cli/src/core/questions/creator/components.ts
@@ -3,6 +3,7 @@ export default {
   type: 'list',
   message: '选择 UI 框架',
   choices: [
+    { name: 'Vuetify3 Beta', value: 'vuetify' },
     { name: 'Element Plus', value: 'element' },
     { name: 'Ant Design Vue 2.x', value: 'antd' },
     { name: 'Naive UI', value: 'naive' },
