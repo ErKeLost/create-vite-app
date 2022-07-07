495c615fbfcf2397f7a0c026a42fc841e7bb3fd5
erkelost <1256029807@qq.com>
2022-07-07 14:01:29 +0800

    ✨ feat(depoly): ✨ feat(css): docs style depoly error
    只能使用 require

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import { h } from 'vue'
 import AsideSponsors from './components/AsideSponsors.vue'
 import HomeSponsors from './components/HomeSponsors.vue'
 import './styles/vars.css'
-
 export default {
   ...Theme,
   Layout() {
