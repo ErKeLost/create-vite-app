a99019d865577c3930eb11560a6377809f9244b4
ERKELSOT <1256029807@qq.com>
2022-07-10 12:45:06 +0800

    ✨ feat(0.1.0-alpha): update 0.1.0版本

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -66,38 +66,38 @@ export default defineConfig({
     },
 
     nav: [
-      { text: 'Guide', link: '/guide/', activeMatch: '/guide/' },
-      { text: 'Config', link: '/config/', activeMatch: '/config/' },
-      { text: 'Plugins', link: '/plugins/', activeMatch: '/plugins/' },
-      {
-        text: 'Links',
-        items: [
-          {
-            text: 'Twitter',
-            link: 'https://twitter.com/vite_js'
-          },
-          {
-            text: 'Discord Chat',
-            link: 'https://chat.vitejs.dev'
-          },
-          {
-            text: 'Awesome Vite',
-            link: 'https://github.com/vitejs/awesome-vite'
-          },
-          {
-            text: 'DEV Community',
-            link: 'https://dev.to/t/vite'
-          },
-          {
-            text: 'Rollup Plugins Compat',
-            link: 'https://vite-rollup-plugins.patak.dev/'
-          },
-          {
-            text: 'Changelog',
-            link: 'https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md'
-          }
-        ]
-      }
+      { text: 'Guide', link: '/guide/', activeMatch: '/guide/' }
+      // { text: 'Config', link: '/config/', activeMatch: '/config/' },
+      // { text: 'Plugins', link: '/plugins/', activeMatch: '/plugins/' },
+      // {
+      //   text: 'Links',
+      //   items: [
+      //     {
+      //       text: 'Twitter',
+      //       link: 'https://twitter.com/vite_js'
+      //     },
+      //     {
+      //       text: 'Discord Chat',
+      //       link: 'https://chat.vitejs.dev'
+      //     },
+      //     {
+      //       text: 'Awesome Vite',
+      //       link: 'https://github.com/vitejs/awesome-vite'
+      //     },
+      //     {
+      //       text: 'DEV Community',
+      //       link: 'https://dev.to/t/vite'
+      //     },
+      //     {
+      //       text: 'Rollup Plugins Compat',
+      //       link: 'https://vite-rollup-plugins.patak.dev/'
+      //     },
+      //     {
+      //       text: 'Changelog',
+      //       link: 'https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md'
+      //     }
+      //   ]
+      // }
       // {
       //   text: 'v3 (next)',
       //   items: [
@@ -114,121 +114,121 @@ export default defineConfig({
         {
           text: 'Guide',
           items: [
-            {
-              text: 'Why Vite',
-              link: '/guide/why'
-            },
+            // {
+            //   text: 'Why Vite',
+            //   link: '/guide/why'
+            // },
             {
               text: 'Getting Started',
               link: '/guide/'
-            },
-            {
-              text: 'Features',
-              link: '/guide/features'
-            },
-            {
-              text: 'Using Plugins',
-              link: '/guide/using-plugins'
-            },
-            {
-              text: 'Dependency Pre-Bundling',
-              link: '/guide/dep-pre-bundling'
-            },
-            {
-              text: 'Static Asset Handling',
-              link: '/guide/assets'
-            },
-            {
-              text: 'Building for Production',
-              link: '/guide/build'
-            },
-            {
-              text: 'Deploying a Static Site',
-              link: '/guide/static-deploy'
-            },
-            {
-              text: 'Env Variables and Modes',
-              link: '/guide/env-and-mode'
-            },
-            {
-              text: 'Server-Side Rendering (SSR)',
-              link: '/guide/ssr'
-            },
-            {
-              text: 'Backend Integration',
-              link: '/guide/backend-integration'
-            },
-            {
-              text: 'Comparisons',
-              link: '/guide/comparisons'
-            },
-            {
-              text: 'Migration from v2',
-              link: '/guide/migration'
             }
-          ]
-        },
-        {
-          text: 'APIs',
-          items: [
-            {
-              text: 'Plugin API',
-              link: '/guide/api-plugin'
-            },
-            {
-              text: 'HMR API',
-              link: '/guide/api-hmr'
-            },
-            {
-              text: 'JavaScript API',
-              link: '/guide/api-javascript'
-            },
-            {
-              text: 'Config Reference',
-              link: '/config/'
-            }
+            // {
+            //   text: 'Features',
+            //   link: '/guide/features'
+            // },
+            // {
+            //   text: 'Using Plugins',
+            //   link: '/guide/using-plugins'
+            // },
+            // {
+            //   text: 'Dependency Pre-Bundling',
+            //   link: '/guide/dep-pre-bundling'
+            // },
+            // {
+            //   text: 'Static Asset Handling',
+            //   link: '/guide/assets'
+            // },
+            // {
+            //   text: 'Building for Production',
+            //   link: '/guide/build'
+            // },
+            // {
+            //   text: 'Deploying a Static Site',
+            //   link: '/guide/static-deploy'
+            // },
+            // {
+            //   text: 'Env Variables and Modes',
+            //   link: '/guide/env-and-mode'
+            // },
+            // {
+            //   text: 'Server-Side Rendering (SSR)',
+            //   link: '/guide/ssr'
+            // },
+            // {
+            //   text: 'Backend Integration',
+            //   link: '/guide/backend-integration'
+            // },
+            // {
+            //   text: 'Comparisons',
+            //   link: '/guide/comparisons'
+            // },
+            // {
+            //   text: 'Migration from v2',
+            //   link: '/guide/migration'
+            // }
           ]
         }
-      ],
-      '/config/': [
-        {
-          text: 'Config',
-          items: [
-            {
-              text: 'Configuring Vite',
-              link: '/config/'
-            },
-            {
-              text: 'Shared Options',
-              link: '/config/shared-options'
-            },
-            {
-              text: 'Server Options',
-              link: '/config/server-options'
-            },
-            {
-              text: 'Build Options',
-              link: '/config/build-options'
-            },
-            {
-              text: 'Preview Options',
-              link: '/config/preview-options'
-            },
-            {
-              text: 'Dep Optimization Options',
-              link: '/config/dep-optimization-options'
-            },
-            {
-              text: 'SSR Options',
-              link: '/config/ssr-options'
-            },
-            {
-              text: 'Worker Options',
-              link: '/config/worker-options'
-            }
-          ]
-        }
+        // {
+        //   text: 'APIs',
+        //   items: [
+        //     {
+        //       text: 'Plugin API',
+        //       link: '/guide/api-plugin'
+        //     },
+        //     {
+        //       text: 'HMR API',
+        //       link: '/guide/api-hmr'
+        //     },
+        //     {
+        //       text: 'JavaScript API',
+        //       link: '/guide/api-javascript'
+        //     },
+        //     {
+        //       text: 'Config Reference',
+        //       link: '/config/'
+        //     }
+        //   ]
+        // }
       ]
+      // '/config/': [
+      //   {
+      //     text: 'Config',
+      //     items: [
+      //       {
+      //         text: 'Configuring Vite',
+      //         link: '/config/'
+      //       },
+      //       {
+      //         text: 'Shared Options',
+      //         link: '/config/shared-options'
+      //       },
+      //       {
+      //         text: 'Server Options',
+      //         link: '/config/server-options'
+      //       },
+      //       {
+      //         text: 'Build Options',
+      //         link: '/config/build-options'
+      //       },
+      //       {
+      //         text: 'Preview Options',
+      //         link: '/config/preview-options'
+      //       },
+      //       {
+      //         text: 'Dep Optimization Options',
+      //         link: '/config/dep-optimization-options'
+      //       },
+      //       {
+      //         text: 'SSR Options',
+      //         link: '/config/ssr-options'
+      //       },
+      //       {
+      //         text: 'Worker Options',
+      //         link: '/config/worker-options'
+      //       }
+      //     ]
+      //   }
+      // ]
     }
   }
 })
