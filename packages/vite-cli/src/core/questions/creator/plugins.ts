86503eef6d6ccef4942f35f333c47954b8f2de95
ERKELSOT <1256029807@qq.com>
2022-07-13 22:02:19 +0800

    ✨ feat(eslint prettier): 新增 prettier eslint 配置 重构 prompt 用户交互问题

--- packages/vite-cli/src/core/questions/creator/plugins.ts
+++ packages/vite-cli/src/core/questions/creator/plugins.ts
@@ -1,28 +1,28 @@
 export default {
   name: 'plugins',
-  type: 'multiselect',
+  type: 'checkbox',
   message: 'Custorm Your Plugins (自定义您的插件)',
   choices: [
-    { title: '@vitejs/plugin-vue-jsx 提供 Vue 3 JSX 支持', value: 'jsx' },
+    { name: '@vitejs/plugin-vue-jsx 提供 Vue 3 JSX 支持', value: 'jsx' },
     {
       name: '@vitejs/plugin-legacy 为打包后的文件提供传统浏览器兼容性支持',
       value: 'legacy'
     },
     {
-      title: 'unplugin-vue-components 自动按需导入Vue组件',
+      name: 'unplugin-vue-components 自动按需导入Vue组件',
       value: 'vue-components'
     },
-    { title: 'unplugin-auto-import 自动引入Api', value: 'auto-import' },
-    { title: 'Unocss 即时的按需原子 CSS 引擎', value: 'unocss' },
-    { title: 'vite-plugin-pwa 零配置 PWA', value: 'pwa' },
+    { name: 'unplugin-auto-import 自动引入Api', value: 'auto-import' },
+    { name: 'Unocss 即时的按需原子 CSS 引擎', value: 'unocss' },
+    { name: 'vite-plugin-pwa 零配置 PWA', value: 'pwa' },
     {
-      title: 'vite-plugin-inspect 模块运行时依赖可视化',
+      name: 'vite-plugin-inspect 模块运行时依赖可视化',
       value: 'inspect'
     },
     {
-      title: 'rollup-plugin-visualizer 打包后包体积分析',
+      name: 'rollup-plugin-visualizer 打包后包体积分析',
       value: 'visualizer'
     },
-    { title: 'unplugin-icons 按需加载图标库', value: 'icons' }
+    { name: 'unplugin-icons 按需加载图标库', value: 'icons' }
   ]
 }
