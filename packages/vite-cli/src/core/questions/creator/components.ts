48cee7dbe36727350b96f60f61a9b176c09fb87c
erkelost <1256029807@qq.com>
2022-07-14 13:25:29 +0800

    ✨ feat(compoennts): add varlet devui library
    只能使用 require

--- packages/vite-cli/src/core/questions/creator/components.ts
+++ packages/vite-cli/src/core/questions/creator/components.ts
@@ -3,10 +3,12 @@ export default {
   name: 'components', //
   message: 'choose UI frameWork (选择您的 UI 框架)',
   choices: [
-    { title: 'Vuetify3 Beta', value: 'vuetify' },
     { title: 'Element Plus', value: 'element-plus' },
-    { title: 'Ant Design Vue 2.x', value: 'ant-design' },
     { title: 'Naive UI', value: 'naive-ui' },
-    { title: 'Vant 3.x', value: 'vant' }
+    { title: 'Ant Design Vue 2.x', value: 'ant-design' },
+    { title: 'DevUI', value: 'devui' },
+    { title: 'Vuetify3 Beta', value: 'vuetify' },
+    { title: 'Vant 3.x', value: 'vant' },
+    { title: 'Varlet', value: 'varlet' }
   ]
 }
