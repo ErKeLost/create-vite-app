d3cb7aa753654298c300ccd382add29876bc1597
erkelost <1256029807@qq.com>
2022-07-19 09:23:16 +0800

    📝 docs(docs): update readme
    只能使用 require

--- packages/vite-cli/src/core/questions/creator/components.ts
+++ packages/vite-cli/src/core/questions/creator/components.ts
@@ -7,8 +7,11 @@ export default {
     { title: 'Naive UI', value: 'naive-ui' },
     { title: 'Ant Design Vue 2.x', value: 'ant-design' },
     { title: 'DevUI', value: 'devui' },
+    { title: 'arco-design', value: 'devui' },
+    { title: 'TDesign', value: 'tencent' },
     { title: 'Vuetify3 Beta', value: 'vuetify' },
     { title: 'Vant 3.x', value: 'vant' },
-    { title: 'Varlet', value: 'varlet' }
+    { title: 'Varlet', value: 'varlet' },
+    { title: 'tdesign-mobile-vue', value: 'tencent-mobile' }
   ]
 }
