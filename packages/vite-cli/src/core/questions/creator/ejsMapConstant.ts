302e620836ae588a77f52f00582d5a355b63d84a
ERKELSOT <1256029807@qq.com>
2022-07-21 06:53:36 +0800

    ✨ feat(refactor): map 映射 测试成功

---
+++ packages/vite-cli/src/core/questions/creator/ejsMapConstant.ts
@@ -0,0 +1,6 @@
+const componentsMap = new Map()
+
+componentsMap.set('element-plus', '"element-plus": "^2.2.5",')
+componentsMap.set('naive-ui', '"naive-ui": "^2.31.0",')
+
+export { componentsMap }
--- packages/vite-cli/src/core/questions/creator/index.ts
+++ packages/vite-cli/src/core/questions/creator/index.ts
@@ -6,6 +6,7 @@ import PackageDevice from './packageDevice'
 import Plugins from './plugins'
 import future from './feature'
 import device from './device'
+import { componentsMap } from './ejsMapConstant'
 // import prompts from 'prompts'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const prompts = require('prompts')
@@ -13,19 +14,46 @@ async function createQuestion(util, question) {
   const result: QuestionCollection = await util(question)
   Object.assign(options, result)
   console.log(result, 'result')
+
   //  在 回答问题得时候 map 映射 每一个 库 版本 问题 要不要考虑
   return Promise.resolve(true)
 }
 
 async function createProjectQuestions(): Promise<void> {
+  // 包管理器版本
   await createQuestion(prompts, PackageDevice)
+  // 新特性 新预设
   await createQuestion(prompts, future)
+  // pc or mobile
   await createQuestion(prompts, device)
+  // ui library
   await createQuestion(prompts, components)
+  // vite plugins
   await createQuestion(prompts, Plugins)
   await createQuestion(prompts, precss)
+  // options 对象属性 所有 属性
   console.log(options)
+  // 获取 选中 components
+  // console.log(componentsMap.get(options.components))
+  const currentLibrary = componentsMap.get(options.components)
+  // 获取整个map
+  // console.log(componentsMap, 'map')
+  // map 转 对象
+  // const res = strMapToObj(componentsMap)
+  // console.log(res)
+  // 合并 map 对象
+  // console.log(Object.assign(options, res), '合并map对象')
+  options.components = currentLibrary
+  console.log(options)
+
   return Promise.resolve()
 }
 
 export default createProjectQuestions
+function strMapToObj(strMap) {
+  const obj = Object.create(null) //创建空的对象
+  for (const [k, v] of strMap) {
+    obj[k] = v
+  }
+  return obj
+}
