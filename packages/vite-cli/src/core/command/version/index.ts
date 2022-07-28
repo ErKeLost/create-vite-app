289e3f83c2895883352af91d60ac0d9675de0843
erkelost <1256029807@qq.com>
2022-07-28 13:00:33 +0800

    ✨ feat(npx): 新增npx 配置 重构命令

--- packages/vite-cli/src/core/command/version/index.ts
+++ packages/vite-cli/src/core/command/version/index.ts
@@ -2,7 +2,6 @@ import { magenta as color } from 'chalk'
 import program from '../../program'
 import { magenta } from '../../../utils/log'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const gradient = require('gradient-string')
 import {
@@ -11,18 +10,21 @@ import {
   VITE_CLI_VERSION,
   BUILD_DATE
 } from '../../../shared/constant'
-import createSpawnCmd from '@/utils/createSpawnCmd'
+function getVersionView() {
+  color(VITE_CLI_VERSION)
+  magenta(gradient.atlas(VALUE_ONLINE))
+  magenta(VERSION)
+  magenta(BUILD_DATE)
+  return ''
+}
 const createVersionCommand = async () => {
   program
-    .version(color(VITE_CLI_VERSION), '-v --version')
+    .version(getVersionView(), '-v --version')
     .usage('<command> [options]')
     .action(() => {
       magenta(gradient.atlas(VALUE_ONLINE))
       magenta(VERSION)
       magenta(BUILD_DATE)
     })
-  const dest = path.resolve(process.cwd())
-  const cmdInherit = createSpawnCmd(dest, 'inherit')
-  // await cmdInherit('vite create asd')
 }
 export default createVersionCommand
