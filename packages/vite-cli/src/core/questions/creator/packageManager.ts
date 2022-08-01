b52569a8bcb55611c63b15370027f912aa655509
ERKELSOT <1256029807@qq.com>
2022-08-01 21:08:48 +0800

    ✨ feat(manager): 新增判断用户是否安装包管理器

--- packages/vite-cli/src/core/questions/creator/packageManager.ts
+++ packages/vite-cli/src/core/questions/creator/packageManager.ts
@@ -1,10 +1,20 @@
+import { shouldUseYarn } from '@/utils/shouldUseYarn'
+import { shouldUsePnpm } from '@/utils/shouldUsePnpm'
+const isYarnInstalled = shouldUseYarn()
+const isPnpmInstalled = shouldUsePnpm()
 export default {
   name: 'package',
   type: 'select',
   message: 'Which package manager do you want to use? (选择您的包管理器)',
   choices: [
-    { title: 'Pnpm', value: 'pnpm' },
-    { title: 'Yarn', value: 'yarn' },
+    {
+      title: isPnpmInstalled ? 'Pnpm' : 'Pnpm (pnpm not install)',
+      value: 'pnpm'
+    },
+    {
+      title: isYarnInstalled ? 'Yarn' : 'Yarn (yarn not install)',
+      value: 'yarn'
+    },
     { title: 'Npm', value: 'npm' }
   ]
 }
