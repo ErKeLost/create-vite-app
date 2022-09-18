37f942576cc5563bc5990768d0164602683bb870
ERKELSOT <1256029807@qq.com>
2022-09-18 19:40:01 +0800

    chore(dev): 开发模式

---
+++ packages/vite-cli/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  target: 'esnext',
+  format: ['esm', 'cjs', 'iife'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  watch: true
+})
