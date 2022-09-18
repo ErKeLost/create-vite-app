a14285308f9f052db342423d3891f2d1d227edbf
ERKELSOT <1256029807@qq.com>
2022-09-18 20:43:00 +0800

    chore(jzzx): remove jzzx-value

--- packages/vite-cli/src/core/command/version/index.ts
+++ packages/vite-cli/src/core/command/version/index.ts
@@ -1,6 +1,5 @@
 import program from '../../program'
 import { magenta } from '../../../utils/log'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 import {
   VERSION,
   VITE_CLI_VERSION,
@@ -8,7 +7,6 @@ import {
 } from '../../../shared/constant'
 function getVersionView() {
   magenta(VITE_CLI_VERSION)
-  // magenta(gradient.atlas(VALUE_ONLINE))
   magenta(VERSION)
   magenta(BUILD_DATE)
   return ''
@@ -18,7 +16,6 @@ const createVersionCommand = async () => {
     .version(getVersionView(), '-v --version')
     .usage('<command> [options]')
     .action(() => {
-      // magenta(gradient.atlas(VALUE_ONLINE))
       magenta(VERSION)
       magenta(BUILD_DATE)
     })
