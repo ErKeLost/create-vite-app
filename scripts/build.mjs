6f762c63674ba3bd517de1fff66bf06e04a47da7
ERKELSOT <1256029807@qq.com>
2022-12-13 22:20:07 +0800

    chore: esbuild chokidar watch file

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,6 +1,6 @@
 import * as esbuild from 'esbuild'
 import chalk from 'chalk'
-const chokidar = require('chokidar')
+import chokidar from 'chokidar'
 
 const watcher = chokidar.watch('file, dir, glob, or array', {
   ignored: /(^|[\/\\])\../,
@@ -35,7 +35,28 @@ await esbuild.build({
           )
         })
         build.onLoad({ filter: /.*/ }, (args) => {
-          console.log(args)
+          watcher
+            .on('add', (path) => console.log(`File ${path} has been added`))
+            .on('change', (path) =>
+              console.log(`File ${path} has been changed`)
+            )
+            .on('unlink', (path) =>
+              console.log(`File ${path} has been removed`)
+            )
+            .on('addDir', (path) =>
+              console.log(`Directory ${path} has been added`)
+            )
+            .on('unlinkDir', (path) =>
+              console.log(`Directory ${path} has been removed`)
+            )
+            .on('error', (error) => console.log(`Watcher error: ${error}`))
+            .on('ready', () =>
+              console.log('Initial scan complete. Ready for changes')
+            )
+            .on('all', (event, path) => console.log(event, path))
+            .on('raw', (event, path, details) => {
+              log('Raw event info:', event, path, details)
+            })
         })
       }
     },
