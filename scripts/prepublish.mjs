a468735d5d32b5a2877c2b7537af3387ae962c5a
erkelost <1256029807@qq.com>
2023-02-17 09:23:53 +0800

    chore: resolve pnpm-lock

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -4,7 +4,7 @@ import 'zx/globals'
 await $`pnpm build`
 await $`pnpm snapshot`
 
-let { version } = JSON.parse(await fs.readFile('./package.json'))
+const { version } = JSON.parse(await fs.readFile('./package.json'))
 
 const playgroundDir = path.resolve(__dirname, '../playground/')
 cd(playgroundDir)
