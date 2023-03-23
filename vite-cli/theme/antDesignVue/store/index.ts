b02b11fc65e5252f551078f61cf1a72abf89dd94
erkelost <1256029807@qq.com>
2023-03-23 09:17:35 +0800

    chore: import word

--- vite-cli/theme/antDesignVue/store/index.ts
+++ vite-cli/theme/antDesignVue/store/index.ts
@@ -1,6 +1,6 @@
 import type { App } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersist from 'pinia-plugin-persist'
+import piniaPluginPersist from 'pinia-plugin-persistedstate'
 const store = createPinia()
 store.use(piniaPluginPersist)
 export function setupStore(app: App<Element>) {
