395ad985131f6548bea34c85c82b5da7f453cd48
erkelost <1256029807@qq.com>
2023-04-15 21:56:23 +0800

    feat: add tiny-vue

--- vite-cli/src/core/questions/creator/index.ts
+++ vite-cli/src/core/questions/creator/index.ts
@@ -20,7 +20,6 @@ async function createProjectQuestions(): Promise<void> {
     // await createQuestion(prompts, device)
     // 根据不同框架继续走不同任务
     await frameQuestions.get(options.frame)()
-    console.log(options)
   } catch (cancelled) {
     // cancel
     console.log(cancelled.message)
